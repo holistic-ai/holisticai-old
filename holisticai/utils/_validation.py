6289339acf76fb44845f153805b6ce1c6b000542
Giulio <giulio.filippi@holisticai.com>
2022-09-21 15:55:05 +0100

    chore: move a function in _validation

--- holisticai/utils/_validation.py
+++ holisticai/utils/_validation.py
@@ -559,3 +559,19 @@ def _multiclass_checks(
             classes = None
 
     return p_attr, y_pred, y_true, groups, classes
+
+
+def _check_valid_y_proba(y_proba: np.ndarray):
+    atol = 1e-3
+    y_shape = np.shape(y_proba)
+    assert len(y_shape) == 2, f"""y_proba must be 2d tensor, found: {y_shape}"""
+
+    sum_all_probs = np.sum(y_proba, axis=1)
+    assert np.all(
+        np.isclose(sum_all_probs, 1, atol=atol)
+    ), f"""probability must sum to 1 across the possible classes, found: {sum_all_probs}"""
+
+    correct_proba_values = np.all(y_proba <= 1) and np.all(y_proba >= 0)
+    assert (
+        correct_proba_values
+    ), f"""probability values must be in the interval [0,1], found: {y_proba}"""
--- holisticai/utils/transformers/bias/_postprocessing.py
+++ holisticai/utils/transformers/bias/_postprocessing.py
@@ -2,7 +2,7 @@ from typing import Optional, Union
 
 import numpy as np
 
-from holisticai.utils._bias_mitigation_validation import check_valid_y_proba
+from holisticai.utils._validation import _check_valid_y_proba
 from holisticai.utils.transformers._transformer_base import BMTransformerBase
 
 
@@ -29,7 +29,7 @@ class BMPostprocessing(BMTransformerBase):
         if "y_proba" in kargs:
             y_proba = np.array(kargs.get("y_proba"))
 
-            check_valid_y_proba(y_proba=y_proba)
+            _check_valid_y_proba(y_proba=y_proba)
 
             params.update({"y_proba": y_proba})
             favorable_index = 1
