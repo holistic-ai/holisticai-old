2666a2eb4b1993965151957de2e51ae563f1d6f3
cristian <cristian.munoz@holisticai.com>
2022-10-13 08:41:04 -0300

    chore: pre-commit

--- holisticai/bias/mitigation/postprocessing/ml_debiaser/reduce2binary/algorithm_utils.py
+++ holisticai/bias/mitigation/postprocessing/ml_debiaser/reduce2binary/algorithm_utils.py
@@ -1,9 +1,14 @@
 from holisticai.bias.mitigation.inprocessing.commons import Logging
 
+
 class RBLogger:
     def __init__(self, total_iterations, verbose):
         self.verbose = verbose
-        log_params = [("iteration", int), ("primal_residual:", float), ("dual_residual:", float)]
+        log_params = [
+            ("iteration", int),
+            ("primal_residual:", float),
+            ("dual_residual:", float),
+        ]
         self.logger = Logging(
             log_params=log_params,
             total_iterations=total_iterations,
