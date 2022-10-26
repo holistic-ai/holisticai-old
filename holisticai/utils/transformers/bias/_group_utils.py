fb69ade58108dcc18962ea72c89c2837a3665537
cristian.munoz <cristian.munoz@holisticai.com>
2022-10-26 11:04:36 -0300

    Bias mitigation part5 (#22)
    * chore: update reweighing bm for multiclass and faster processing
    
    * chore: add tutorials for multiclass and binary classification bm
    
    * chore: pre-commit

--- holisticai/utils/transformers/bias/_group_utils.py
+++ holisticai/utils/transformers/bias/_group_utils.py
@@ -9,16 +9,18 @@ class SensitiveGroups:
 
     def fit(self, sensitive_features):
         self.tags = pd.DataFrame()
-        group_ids = self.merge_columns(sensitive_features)
-        _, unique_group_id = pd.factorize(group_ids)
-        self.group2num = {g: i for i, g in enumerate(unique_group_id)}
+        group_lbs = self.merge_columns(sensitive_features)
+        _, unique_group_lbs = pd.factorize(group_lbs)
+        self.num_groups = len(unique_group_lbs)
+        self.group_names = list(unique_group_lbs)
+        self.group2num = {g: i for i, g in enumerate(unique_group_lbs)}
         return self
 
     def transform(self, sensitive_features, convert_numeric=False):
-        group_ids = self.merge_columns(sensitive_features)
+        group_lbs = self.merge_columns(sensitive_features)
         if convert_numeric:
-            return group_ids.apply(lambda x: self.group2num[x])
-        return group_ids
+            return group_lbs.apply(lambda x: self.group2num[x])
+        return group_lbs
 
     def fit_transform(self, sensitive_features, convert_numeric=False):
         return self.fit(sensitive_features).transform(
