684be69bc797f651f33831fe8337492892c52a8a
cristian <cristian.munoz@holisticai.com>
2022-11-22 22:35:37 -0300

    chore: pre-commit

--- holisticai/bias/mitigation/inprocessing/matrix_factorization/debiasing_learning/algorithm.py
+++ holisticai/bias/mitigation/inprocessing/matrix_factorization/debiasing_learning/algorithm.py
@@ -1,89 +1,111 @@
 import numpy as np
 import scipy
+
 from .algorithm_utils import (
-    init_random_parameters, 
-    Problem,  
-    get_metric_mode, 
-    get_bias_configuration, 
     ParameterSerializer,
-    process_propensities,
+    Problem,
+    get_bias_configuration,
+    get_metric_mode,
+    init_random_parameters,
     normalize_propensities,
-    predict_scores
+    predict_scores,
+    process_propensities,
 )
 
+
 class DebiasingLearningAlgorithm:
-	def __init__(self, K, normalization, lamda, metric, bias_mode, clip_val=-1, verbose=0):
-		self.normalization = normalization
-		self.metric = metric
-		self.K = K
-		self.clip_val = clip_val
-		self.lamda = lamda
-		self.bias_mode = bias_mode
-		self.verbose = verbose
-    
-	def train(self, train_observations, inv_propensities, start_vector=None):
-		if start_vector is not None:
-			assert start_vector[0].shape[1] == self.K
-			assert start_vector[1].shape[1] == self.K
+    def __init__(
+        self, K, normalization, lamda, metric, bias_mode, clip_val=-1, verbose=0
+    ):
+        self.normalization = normalization
+        self.metric = metric
+        self.K = K
+        self.clip_val = clip_val
+        self.lamda = lamda
+        self.bias_mode = bias_mode
+        self.verbose = verbose
 
-		tempInvPropensities = None
-		if inv_propensities is not None:
-			tempInvPropensities = (4.0 / 3.0) * inv_propensities
-			if self.clip_val >= 0:
-				tempInvPropensities = np.clip(tempInvPropensities, a_min = 0, a_max = self.clip_val)
+    def train(self, train_observations, inv_propensities, start_vector=None):
+        if start_vector is not None:
+            assert start_vector[0].shape[1] == self.K
+            assert start_vector[1].shape[1] == self.K
 
-		parameters = self.optimize_debiasing_parameters(train_observations, 
-                                              tempInvPropensities, 
-                                              self.lamda,
-									start_vec = start_vector)
-		return parameters
+        tempInvPropensities = None
+        if inv_propensities is not None:
+            tempInvPropensities = (4.0 / 3.0) * inv_propensities
+            if self.clip_val >= 0:
+                tempInvPropensities = np.clip(
+                    tempInvPropensities, a_min=0, a_max=self.clip_val
+                )
 
-	def optimize_debiasing_parameters(self, observed_ratings, 
-                                   inverse_propensities, 
-                                   l2_regularization, 
-                                   start_vec = None):
+        parameters = self.optimize_debiasing_parameters(
+            train_observations, tempInvPropensities, self.lamda, start_vec=start_vector
+        )
+        return parameters
 
-		metricMode = get_metric_mode(self.metric)
-		numUsers, numItems = np.shape(observed_ratings)
-		scale = numUsers * numItems
-		useBias,regularizeBias = get_bias_configuration(self.bias_mode)
-		serializer = ParameterSerializer(numUsers=numUsers, 
-                                   numItems=numItems, 
-                                   num_dimensions=self.K)
-		config = {
-			'numUsers':numUsers,
-			'numItems':numItems,
-			'scale':scale,
-			'useBias':useBias,
-			'regularizeBias':regularizeBias,
-			'metricMode':metricMode,
-			'num_dimensions':self.K,
-			'l2_regularization':l2_regularization,
-			'serializer':serializer
-		}
-		
-		inversePropensities = process_propensities(observed_ratings, inverse_propensities)
-		normalized_propensities = normalize_propensities(inversePropensities, self.normalization, scale, numItems, numUsers)
-		normalized_propensities = np.ma.filled(normalized_propensities, 0.0)
-		observed_ratings = np.ma.filled(observed_ratings, 0)
-		
-		problem = Problem(observed_ratings=observed_ratings, 
-						normalized_propensities=normalized_propensities, 
-						config=config)
+    def optimize_debiasing_parameters(
+        self, observed_ratings, inverse_propensities, l2_regularization, start_vec=None
+    ):
 
-		startparameters = init_random_parameters(config, start_vec=start_vec)
-		startVector = serializer.serialize(*startparameters)
+        metricMode = get_metric_mode(self.metric)
+        numUsers, numItems = np.shape(observed_ratings)
+        scale = numUsers * numItems
+        useBias, regularizeBias = get_bias_configuration(self.bias_mode)
+        serializer = ParameterSerializer(
+            numUsers=numUsers, numItems=numItems, num_dimensions=self.K
+        )
+        config = {
+            "numUsers": numUsers,
+            "numItems": numItems,
+            "scale": scale,
+            "useBias": useBias,
+            "regularizeBias": regularizeBias,
+            "metricMode": metricMode,
+            "num_dimensions": self.K,
+            "l2_regularization": l2_regularization,
+            "serializer": serializer,
+        }
 
-		ops = {'maxiter': 2000, 'disp': False, 'gtol': 1e-5,\
-				'ftol': 1e-5, 'maxcor': 50, 'disp':False}
+        inversePropensities = process_propensities(
+            observed_ratings, inverse_propensities
+        )
+        normalized_propensities = normalize_propensities(
+            inversePropensities, self.normalization, scale, numItems, numUsers
+        )
+        normalized_propensities = np.ma.filled(normalized_propensities, 0.0)
+        observed_ratings = np.ma.filled(observed_ratings, 0)
 
-		result = scipy.optimize.minimize(fun = problem.evaluate, x0 = startVector,
-						method = 'L-BFGS-B', jac = True, tol = 1e-5, options = ops)
-		
-		return serializer.deserialize(result['x'])
+        problem = Problem(
+            observed_ratings=observed_ratings,
+            normalized_propensities=normalized_propensities,
+            config=config,
+        )
 
-	def predict(self, model_params, bias_mode):
-		use_bias = True
-		if bias_mode == 'None':
-			use_bias = False
-		return predict_scores(*model_params, use_bias=use_bias)
+        startparameters = init_random_parameters(config, start_vec=start_vec)
+        startVector = serializer.serialize(*startparameters)
+
+        ops = {
+            "maxiter": 2000,
+            "disp": False,
+            "gtol": 1e-5,
+            "ftol": 1e-5,
+            "maxcor": 50,
+            "disp": False,
+        }
+
+        result = scipy.optimize.minimize(
+            fun=problem.evaluate,
+            x0=startVector,
+            method="L-BFGS-B",
+            jac=True,
+            tol=1e-5,
+            options=ops,
+        )
+
+        return serializer.deserialize(result["x"])
+
+    def predict(self, model_params, bias_mode):
+        use_bias = True
+        if bias_mode == "None":
+            use_bias = False
+        return predict_scores(*model_params, use_bias=use_bias)
--- holisticai/bias/mitigation/inprocessing/matrix_factorization/non_negative.py
+++ holisticai/bias/mitigation/inprocessing/matrix_factorization/non_negative.py
@@ -1,19 +1,24 @@
-import numpy as np
-from sklearn.decomposition import NMF
 from typing import Optional
 
-class NonNegativeMF:
+import numpy as np
+from sklearn.decomposition import NMF
+
+from holisticai.utils.transformers.bias import BMInprocessing as BMImp
+
+
+class NonNegativeMF(BMImp):
     """
-    This model is trained using conventional matrix factorization (Eq.1 [1] with Pu,i = 1), 
-    and the system always selects the top predicted item for each user, and adds 
+    This model is trained using conventional matrix factorization (Eq.1 [1] with Pu,i = 1),
+    and the system always selects the top predicted item for each user, and adds
     it to the next (new) training set.
 
     References:
-        [1] Sun, Wenlong, et al. "Debiasing the human-recommender system feedback loop in 
-        collaborative filtering." Companion Proceedings of The 2019 World Wide Web 
-        Conference. 2019.       
+        [1] Sun, Wenlong, et al. "Debiasing the human-recommender system feedback loop in
+        collaborative filtering." Companion Proceedings of The 2019 World Wide Web
+        Conference. 2019.
     """
-    def __init__(self, K: Optional[int]=10):
+
+    def __init__(self, K: Optional[int] = 10):
         """
         Init Wrapper Non Negative Matrix Factorization
 
@@ -35,7 +40,7 @@ class NonNegativeMF:
         X : matrix-like
             rating matrix, 0 means non-raked cases.
         """
-        model = NMF(n_components=self.K, init='random', max_iter=200)
+        model = NMF(n_components=self.K, init="random", max_iter=200)
         P = model.fit_transform(X)
         Q = model.components_
         self.pred = np.dot(P, Q)
