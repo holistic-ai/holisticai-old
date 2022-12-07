bde36ecb590bfabdc7444b8bd1d3b242ab36fa71
cristian <cristian.munoz@holisticai.com>
2022-12-07 13:52:33 -0300

    feat: new two sided fairness bias mitigation inprocessing.

--- holisticai/bias/mitigation/inprocessing/two_sided_fairness/algorithm.py
+++ holisticai/bias/mitigation/inprocessing/two_sided_fairness/algorithm.py
@@ -1,31 +1,28 @@
 import math
-
 import numpy as np
+from tqdm import tqdm
 
 
 class FairRecAlg:
-    def __init__(self, rec_size=10, MMS_fraction=0.5):
+    def __init__(self, rec_size=10, MMS_fraction=0.5, verbose=0):
         self.rec_size = rec_size
         self.MMS_fraction = MMS_fraction
-
-    def fit(self, X):
+        self.verbose = verbose
+    
+    def rank(self, X):
         self.m = X.shape[0]
         self.n = X.shape[1]
-        U = range(self.m)
-        P = range(self.n)
-        A = {}
-        for u in U:
-            A[u] = []
-        F = {}
-        for u in U:
-            F[u] = P[:]
+        U = list(range(self.m))
+        P = list(range(self.n))
+        A = {u:[] for u in U}
+        F = {u:P[:] for u in U}
         l = int(self.MMS_fraction * self.m * self.rec_size / (self.n + 0.0))
         R = int(math.ceil((l * self.n) / (self.m + 0.0)))
         T = l * self.n
         [B, F1] = self._greedy_round_robin(R, l, T, X, U[:], F.copy())
-        F = {}
+        
         F = F1.copy()
-        print("GRR done")
+        
         for u in U:
             A[u] = A[u][:] + B[u][:]
         u_less = []
@@ -44,19 +41,20 @@ class FairRecAlg:
         return A
 
     def _greedy_round_robin(self, R, l, T, V, U, F):
-        """greedy round robin allocation based on a specific ordering of
+        """
+        Description
+        -----------
+        Greedy round robin allocation based on a specific ordering of
         customers (assuming the ordering is done in the relevance scoring
-        matrix before passing it here)"""
+        matrix before passing it here)
+        """
 
-        B = {}
-        for u in U:
-            B[u] = []
+        B = {u:[] for u in U}
         Z = {}
-        P = range(self.n)
+        P = list(range(self.n))
         for p in P:
             Z[p] = l
         for t in range(1, R + 1):
-            print("GRR round number: ", t)
             for i in range(self.m):
                 if T == 0:
                     return B, F
--- holisticai/bias/mitigation/inprocessing/two_sided_fairness/transformer.py
+++ holisticai/bias/mitigation/inprocessing/two_sided_fairness/transformer.py
@@ -1,9 +1,8 @@
-import numpy as np
-
 from holisticai.utils.transformers.bias import BMInprocessing as BMImp
-
+from typing import Optional
 from .algorithm import FairRecAlg
-
+import numpy as np
+import pandas as pd
 
 class FairRec(BMImp):
     """
@@ -16,7 +15,9 @@ class FairRec(BMImp):
         recommendations in two-sided platforms." Proceedings of The Web Conference 2020. 2020.
     """
 
-    def __init__(self, rec_size=10, MMS_fraction=0.5):
+    def __init__(self, 
+                 rec_size : Optional[int] = 10, 
+                 MMS_fraction : Optional[float] = 0.5):
         """
         Init FairRec algorithm
         Parameters
@@ -26,25 +27,40 @@ class FairRec(BMImp):
         MMS_fraction : float
             Maximin Share (MMS) threshold of producers exposure.
         """
-        self.rec_size = int(rec_size)
-        self.MMS_fraction = float(MMS_fraction)
+        
+        self.rec_size = rec_size
+        self.MMS_fraction = MMS_fraction
 
     def fit(self, X):
+        algorithm = FairRecAlg(rec_size=self.rec_size, MMS_fraction=self.MMS_fraction)
+        self.recommendation = algorithm.rank(X)
+        return self
+
+
+    def predict(self, X: Optional[np.ndarray], top_n: Optional[int] = None):
         """
         Fit model
+        
         Parameters
         ----------
         X : matrix-like
             scored matrix, 0 means non-raked cases.
+            
         Returns
         -------
         recommendations : dict
             A dictionary of recommendations for each user.
         """
-        self.model_ = FairRecAlg(rec_size=self.rec_size, MMS_fraction=self.MMS_fraction)
-        recommendation = self.model_.fit(X)
-        return recommendation
-
-    def transform_estimator(self, estimator):
-        self.estimator = estimator
-        return self
+        if top_n is None:
+            algorithm = FairRecAlg(rec_size=self.rec_size, MMS_fraction=self.MMS_fraction)
+            self.recommendation = algorithm.rank(X)
+        
+        dfs = []
+        for i,key in enumerate(self.recommendation.keys()):
+            df = pd.DataFrame()
+            df["Y"] = np.array(self.recommendation[key])
+            df["X"] = i
+            df["score"] = 1.0
+            dfs.append(df)
+        dfs = pd.concat(dfs, axis=0)
+        return dfs[["X", "Y", "score"]]
