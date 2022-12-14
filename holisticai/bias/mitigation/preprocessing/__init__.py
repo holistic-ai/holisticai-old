26b780e0d2b61623a6631c0361b40be9e0ed2c44
cristian.munoz <cristian.munoz@holisticai.com>
2022-12-14 10:32:55 -0300

    Recommender system part3 (#48)
    * feat: new feature disparate impact remover for preprocessing and recommender system
    
    * feat: tutorials for bias mitigation  disparate impact remover
    
    * chore: pre-commit
    
    * chore: tutorial using disparate impact remover for preprocessing bias mitigaton

--- holisticai/bias/mitigation/preprocessing/__init__.py
+++ holisticai/bias/mitigation/preprocessing/__init__.py
@@ -10,3 +10,11 @@ __all__ = [
     "CorrelationRemover",
     "FairletClusteringPreprocessing",
 ]
+
+import importlib
+
+networkx_spec = importlib.util.find_spec("networkx")
+if networkx_spec is not None:
+    from .disparate_impact_remover import DisparateImpactRemover
+
+__all__ += ["DisparateImpactRemover"]
