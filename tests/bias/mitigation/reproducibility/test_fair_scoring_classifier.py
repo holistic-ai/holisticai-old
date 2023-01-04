49adb18b08de234744838c34b727c459747bdea3
Franklin <franklin.fernandez@holisticai.com>
2023-01-04 19:32:02 -0300

    fix: minor updates

--- tests/bias/mitigation/reproducibility/test_fair_scoring_classifier.py
+++ tests/bias/mitigation/reproducibility/test_fair_scoring_classifier.py
@@ -27,42 +27,43 @@ def ohot_encoding(df):
                 new_df = pd.concat([new_df, df[col]], axis=1)
     return new_df
 
-df = load_student()['frame']
+def test_fair_scoring_classifier():
+    df = load_student()['frame']
 
-# Make data multiclass by slicing into 4 buckets
-y = df['G3'].to_numpy()
-buckets = np.array([8, 11, 14])
-y_cat = (y.reshape(-1, 1) > buckets.reshape(1, -1)).sum(axis=1)
-df['target'] = y_cat
+    # Make data multiclass by slicing into 4 buckets
+    y = df['G3'].to_numpy()
+    buckets = np.array([8, 11, 14])
+    y_cat = (y.reshape(-1, 1) > buckets.reshape(1, -1)).sum(axis=1)
+    df['target'] = y_cat
 
-# map dictionary
-grade_dict = {0:'very-low', 1:'low', 2:'high',3:'very-high'}
-df['target'] = df['target'].map(grade_dict)
+    # map dictionary
+    grade_dict = {0:'very-low', 1:'low', 2:'high',3:'very-high'}
+    df['target'] = df['target'].map(grade_dict)
 
-# drop the other grade columns
-df = df.drop(columns=['G1','G2','G3'])
+    # drop the other grade columns
+    df = df.drop(columns=['G1','G2','G3'])
 
-df = ohot_encoding(df)
+    df = ohot_encoding(df)
 
-labels_name = list(df.iloc[:,-4:].columns)
+    labels_name = list(df.iloc[:,-4:].columns)
 
-train, test = train_test_split(df, test_size=0.4, random_state=42)
+    train, test = train_test_split(df, test_size=0.4, random_state=42)
 
-X_train = train.drop(columns=labels_name)
-X_test = test.drop(columns=labels_name)
-y_train = train[labels_name]
-y_test = test[labels_name]
+    X_train = train.drop(columns=labels_name)
+    X_test = test.drop(columns=labels_name)
+    y_train = train[labels_name]
+    y_test = test[labels_name]
 
-objectives = "ba"
-constraints = {}
-protected_attributes = ["sex"]
-protected_labels = ["target_very-high"]
+    objectives = "ba"
+    constraints = {}
+    protected_attributes = ["sex"]
+    protected_labels = ["target_very-high"]
 
-numUsers, _ = X_test.shape
-numLabels = len(labels_name)
+    numUsers, _ = X_test.shape
+    numLabels = len(labels_name)
 
-model = FairScoreClassifier(objectives, constraints)
+    model = FairScoreClassifier(objectives, constraints)
 
-model.fit(X_train, y_train, protected_attributes, protected_labels)
+    model.fit(X_train, y_train, protected_attributes, protected_labels)
 
-assert model.predict(X_test).shape == (numUsers, numLabels)
+    assert model.predict(X_test).shape == (numUsers, numLabels)
